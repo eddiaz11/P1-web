e8b80c36c23be7cbfca8c954e677ba2ef8508aea
Ctrl Industries <ed.diaz11@uniandes.edu.co>
2021-03-14 13:17:26 -0500

    Complete rendering

--- js/script.js
+++ js/script.js
@@ -3,21 +3,57 @@ var carrito={};
 
 const url='https://gist.githubusercontent.com/josejbocanegra/9a28c356416badb8f9173daf36d1460b/raw/5ea84b9d43ff494fcbf5c5186544a18b42812f09/restaurant.json';
 
-function addToCar(item){
 
-}
- cargar=()=>{
+
+var load = (url, callback)=>{
     fetch(url)
-    .then(data=> {data.json()})
-    .then((res) => {
-        console.log(res);
+    .then(res => res.json())
+    .then((data) => {
+        if (data) {
+            callback(data);
+        } else {
+            callback(null);
+        }
     })
+    .catch(err => { throw err });    
 }
 
-cargar();
 
-function pintar(){
-    array.forEach(element => {
-        <button className="button-home">Add to car</button>
+var nothing =(data)=>{
+    console.log(data);
+}
+
+
+
+const renderMenu = (data) => {
+    data.forEach(element => {
+        const html = element.products.map((item) =>
+    `
+    <div class="card mb-3">
+    <img src="${item.image}" class="card-img-top" alt="${item.name}">
+    <div class="card-body">
+    <h5 class="card-title">${item.name}</h5>
+    <p class="card-text">
+    ${item.description}
+    </p>
+    <a href="#" class="btn btn-primary">add</a>
+    </div>
+    </div>
+    `
+    ).join(" ");
+    document.getElementById(element.name).innerHTML = html;  
     });
-}
+    
+};
+
+load(url, renderMenu);
+
+const handleAdd = (evt) => {
+    evt.preventDefault();
+    const message = document.getElementById("message");
+    ws.send(message.value);
+    message.value = "";
+};
+
+//const btn = document.getElementById("form");
+//form.addEventListener("submit", handleSubmit);
