333a0a1dbc0b8c3390129258298bf0414bd58b46
Ctrl Industries <ed.diaz11@uniandes.edu.co>
2021-03-14 12:52:21 -0500

    organizando

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ var carrito={};
 
 const url='https://gist.githubusercontent.com/josejbocanegra/9a28c356416badb8f9173daf36d1460b/raw/5ea84b9d43ff494fcbf5c5186544a18b42812f09/restaurant.json';
 
-function addToCar($id){
+function addToCar(item){
 
 }
  cargar=()=>{
